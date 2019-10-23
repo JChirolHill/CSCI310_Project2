94e8451b38cb8305954fdaf389d43d1a8d7b48ff
J Hill <chirolhi@usc.edu>
2019-10-23 16:18:38 -0700

    tiny changes

--- app/src/main/java/projects/chirolhill/juliette/csci310_project2/ShopInfoActivity.java
+++ app/src/main/java/projects/chirolhill/juliette/csci310_project2/ShopInfoActivity.java
@@ -6,8 +6,12 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ShopInfoActivity extends AppCompatActivity {
+    private TextView textShopName;
+    private ImageView imageShopImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +20,9 @@ public class ShopInfoActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        textShopName = findViewById(R.id.textShopName);
+
+        textShopName.setText("asdioghps");
     }
 
 }
