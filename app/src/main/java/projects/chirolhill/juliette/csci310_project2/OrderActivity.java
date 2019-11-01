c8a567625e4e3c4a35ba90d570da91190896ac49
natekong <yichunko@usc.edu>
2019-11-01 14:28:16 -0700

    readonly/editable & starting code in order activity

--- app/src/main/java/projects/chirolhill/juliette/csci310_project2/OrderActivity.java
+++ app/src/main/java/projects/chirolhill/juliette/csci310_project2/OrderActivity.java
@@ -1,15 +1,20 @@
 package projects.chirolhill.juliette.csci310_project2;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
@@ -20,7 +25,20 @@ import projects.chirolhill.juliette.csci310_project2.model.Drink;
 import projects.chirolhill.juliette.csci310_project2.model.Order;
 
 public class OrderActivity extends AppCompatActivity {
-    private static final String TAG = ProfileActivity.class.getSimpleName();
+    private static final String TAG = OrderActivity.class.getSimpleName();
+
+    public static final String EXTRA_READONLY = "order_extra_view"; // display as view or update
+
+    private TextView textOrderTitle;
+    private TextView totalMoneySpentPrompt;
+    private TextView totalMoneySpent;
+    private EditText editTotalMoneySpent;
+    private TextView caffeineLevelPrompt;
+    private TextView caffeineLevel;
+    private EditText editCaffeineLevel;
+    private ListView listOrders;
+
+    private boolean readonly;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,41 +47,83 @@ public class OrderActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-    }
+        textOrderTitle = findViewById(R.id.textOrderTitle);
+        totalMoneySpentPrompt = findViewById(R.id.totalMoneySpentPrompt);
+        totalMoneySpent = findViewById(R.id.textTotalMoneySpent);
+        editTotalMoneySpent = findViewById(R.id.editTotalMoneySpent);
+        caffeineLevelPrompt = findViewById(R.id.textCaffeineLevelPrompt);
+        caffeineLevel = findViewById(R.id.textCaffeineLevel);
+        editCaffeineLevel = findViewById(R.id.editCaffeineLevel);
+        listOrders = findViewById(R.id.listOrders);
 
-    private class DrinkListAdapter extends ArrayAdapter<Drink> {
-        public DrinkListAdapter(Context context, int resource, List<Drink> objects) {
-            super(context, resource, objects);
+        // either from shopinfoactivity -> readonly (review order)
+        // or from profileactivity -> readonly (view/edit past orders)
+        Intent i = getIntent();
+        readonly = i.getBooleanExtra(EXTRA_READONLY, true);
+
+        // render appropriately depending on the readonly state
+        if(readonly) {
+            renderReadOnly();
+        }
+        else {
+            renderEditable();
         }
 
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            if(convertView == null) {
-                convertView = getLayoutInflater().inflate(R.layout.list_view_row, null);
+        listOrders.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                if(!readonly) {
+                    // do stuff, else do nothing
+                }
             }
-
-            TextView textDrinkName = convertView.findViewById(R.id.listDrinkName);
-            TextView textQuantity = convertView.findViewById(R.id.listQuantity);
-            TextView textPrice = convertView.findViewById(R.id.listPrice);
-            TextView textCaffeine = convertView.findViewById(R.id.listCaffeine);
-
-            Drink d = getItem(position);
-
-            // copy/map the data from the current item (model) to the curr row (view)
-
-            // confused here: I need to calculate the quantity/price/caffeine for multiple drinks
-            // with a same drink name within a order, is this correct? So I need to sum up these
-            // values for a drink. But this doesn't make sense for me since I do not have access to
-            // the order if I choose to do a "DrinkListAdapter"
-
-            textDrinkName.setText(d.getName());
-            textQuantity.setText();
-            textPrice.setText(s.getPriceRange());
-            textAddress.setText(s.getAddress());
-            Picasso.get().load(s.getImgURL()).into(image);
-
-            return convertView;
-        }
+        });
     }
 
+    private void renderReadOnly() {
+        textOrderTitle.setText(getResources().getString(R.string.viewOrder));
+        totalMoneySpent.setVisibility(View.VISIBLE);
+        editTotalMoneySpent.setVisibility(View.GONE);
+        caffeineLevel.setVisibility(View.VISIBLE);
+        editCaffeineLevel.setVisibility(View.GONE);
+    }
+
+    private void renderEditable() {
+        textOrderTitle.setText(getResources().getString(R.string.editOrder));
+        totalMoneySpent.setVisibility(View.GONE);
+        editTotalMoneySpent.setVisibility(View.VISIBLE);
+        caffeineLevel.setVisibility(View.GONE);
+        editCaffeineLevel.setVisibility(View.VISIBLE);
+    }
+
+//    private class DrinkListAdapter extends ArrayAdapter<Drink> {
+//        public DrinkListAdapter(Context context, int resource, List<Drink> objects) {
+//            super(context, resource, objects);
+//        }
+//
+//        @Override
+//        public View getView(int position, View convertView, ViewGroup parent) {
+//            if(convertView == null) {
+//                convertView = getLayoutInflater().inflate(R.layout.list_view_row, null);
+//            }
+//
+//            TextView textDrinkName = convertView.findViewById(R.id.listDrinkName);
+//            TextView textQuantity = convertView.findViewById(R.id.listQuantity);
+//            TextView textPrice = convertView.findViewById(R.id.listPrice);
+//            TextView textCaffeine = convertView.findViewById(R.id.listCaffeine);
+//
+//            Drink d = getItem(position);
+//
+//            // copy/map the data from the current item (model) to the curr row (view)
+//
+//            // confused here: I need to git
+//
+//            textDrinkName.setText(d.getName());
+//            textQuantity.setText();
+//            textPrice.setText(s.getPriceRange());
+//            textAddress.setText(s.getAddress());
+//            Picasso.get().load(s.getImgURL()).into(image);
+//
+//            return convertView;
+//        }
+//    }
 }
