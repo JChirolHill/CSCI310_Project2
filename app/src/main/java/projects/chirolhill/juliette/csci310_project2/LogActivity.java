92a2ec463ff1b99449c5800d704d265d9d78c4d6
katherinesing <ksing@usc.edu>
2019-11-02 15:44:50 -0700

    juliette review

--- app/src/main/java/projects/chirolhill/juliette/csci310_project2/LogActivity.java
+++ app/src/main/java/projects/chirolhill/juliette/csci310_project2/LogActivity.java
@@ -17,15 +17,6 @@ public class LogActivity extends AppCompatActivity {
         setContentView(R.layout.activity_log);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
