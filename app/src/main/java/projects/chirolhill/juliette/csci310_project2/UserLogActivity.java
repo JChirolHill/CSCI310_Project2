facd25cf7b179f249aef6c16bd671c2dd3947e4d
Nathan Chang <nathan98chang@gmail.com>
2019-11-04 14:38:35 -0800

    refactored (put chart boilerplate in separate methods) and starting setup for money XY plot

--- app/src/main/java/projects/chirolhill/juliette/csci310_project2/UserLogActivity.java
+++ app/src/main/java/projects/chirolhill/juliette/csci310_project2/UserLogActivity.java
@@ -1,37 +1,23 @@
 package projects.chirolhill.juliette.csci310_project2;
 
 
-import java.text.NumberFormat;
-
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.widget.ImageView;
 import android.widget.ListView;
 
 import com.androidplot.util.PixelUtils;
 import com.androidplot.xy.BarFormatter;
 import com.androidplot.xy.BarRenderer;
 import com.androidplot.xy.BoundaryMode;
-import com.androidplot.xy.SimpleXYSeries;
 import com.androidplot.xy.StepMode;
 import com.androidplot.xy.XYGraphWidget;
 import com.androidplot.xy.XYPlot;
-import com.androidplot.xy.XYSeries;
 
-import java.text.DateFormatSymbols;
 import java.text.FieldPosition;
 import java.text.Format;
 import java.text.ParsePosition;
-import java.util.Arrays;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 
@@ -41,11 +27,10 @@ public class UserLogActivity extends AppCompatActivity {
 
     private final String TAG = UserLogActivity.class.getSimpleName();
 
-    private XYPlot caffeineChart;
+    private XYPlot caffeineBarChart;
+    private XYPlot moneyXYPlot;
     private BarRenderer renderer;
-    private ListView listOrders;
-    private ImageView caffeineChartView;
-    private ImageView moneyChart;
+    private ListView ordersList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,27 +38,43 @@ public class UserLogActivity extends AppCompatActivity {
         setContentView(R.layout.activity_user_log);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        listOrders = findViewById(R.id.listOrders);
-        moneyChart = findViewById(R.id.imageMoneyBarChart);
-        caffeineChartView = findViewById(R.id.imageCaffeineBarChart);
+        ordersList = findViewById(R.id.ordersList);
 
-        // get all orders executed within the past week for the given user
+        // get all orders for user (up to past 50)
 
+        // caffeineBarChart: extract dates and caffeine levels from orders
 
-        // extract dates and caffeine levels from orders
+        // caffeineBarChart: convert data to custom XYSeries, CaffeineLevelSeries
 
+        // moneyXYPlot: extract dates and money levels from orders
 
-        // convert to custom XYSeries, CaffeineLevelSeries
+        // moneyXYPlot: convert data to SimpleXYSeries
 
+        // create charts
+        caffeineBarChart = findViewById(R.id.caffeineBarChart);
+        moneyXYPlot = findViewById(R.id.moneyXYPlot);
 
-        // begin creating chart
-        caffeineChart = findViewById(R.id.caffeineChart);
+        // setup charts
+        // TODO: pass in the data extracted from DB
+        this.onCreateCaffeineChart(caffeineBarChart);
+        this.onCreateMoneyXYPlot(moneyXYPlot);
+
+        // TODO: set up the money chart
+
+        // TODO: set up the list of logs
+    }
+
+    /**
+     * Setup code for the caffeine chart (formatting, incorporating data)
+     * @param caffeineBarChart
+     */
+    public void onCreateCaffeineChart(XYPlot caffeineBarChart) {
 
         // formatting: set y-axis increments from 0 to 1000 in increments of 200, and add extra
         // vals at beg/end to create a margin for the bars
-        caffeineChart.setRangeBoundaries(new Integer(0), new Integer(1000), BoundaryMode.FIXED);
-        caffeineChart.setRangeStep(StepMode.INCREMENT_BY_VAL, 200);
-        caffeineChart.setDomainStep(StepMode.SUBDIVIDE, 9);
+        caffeineBarChart.setRangeBoundaries(new Integer(0), new Integer(1000), BoundaryMode.FIXED);
+        caffeineBarChart.setRangeStep(StepMode.INCREMENT_BY_VAL, 200);
+        caffeineBarChart.setDomainStep(StepMode.SUBDIVIDE, 9);
 
         // Y-AXIS (Daily Caffeine Level, in mg)
         // X-AXIS (Date, e.g. "10/27", previous 7 days)
@@ -97,15 +98,15 @@ public class UserLogActivity extends AppCompatActivity {
         BarFormatter bfCoffee = new BarFormatter(Color.parseColor("#ac9782"), Color.WHITE);
         BarFormatter bfTea = new BarFormatter(Color.parseColor("#4d7d55"), Color.WHITE);
 
-        caffeineChart.addSeries(coffeeSeries, bfCoffee);
-        caffeineChart.addSeries(teaSeries, bfTea);
+        caffeineBarChart.addSeries(coffeeSeries, bfCoffee);
+        caffeineBarChart.addSeries(teaSeries, bfTea);
 
         // initialize bar renderer (must be done after set formatter and add series to the plot)
-        renderer = caffeineChart.getRenderer(BarRenderer.class);
+        renderer = caffeineBarChart.getRenderer(BarRenderer.class);
         renderer.setBarGroupWidth(BarRenderer.BarGroupWidthMode.FIXED_WIDTH, PixelUtils.dpToPix(25));
         renderer.setBarOrientation(BarRenderer.BarOrientation.STACKED);
 
-        caffeineChart.getGraph().getLineLabelStyle(XYGraphWidget.Edge.BOTTOM).setFormat(new Format() {
+        caffeineBarChart.getGraph().getLineLabelStyle(XYGraphWidget.Edge.BOTTOM).setFormat(new Format() {
             // converts the Long received from the X-axis value into a date, and then neatly formats it
             @Override
             public StringBuffer format(Object obj, StringBuffer toAppendTo, FieldPosition pos) {
@@ -124,7 +125,7 @@ public class UserLogActivity extends AppCompatActivity {
             }
         });
 
-        caffeineChart.getGraph().getLineLabelStyle(XYGraphWidget.Edge.LEFT).setFormat(new Format() {
+        caffeineBarChart.getGraph().getLineLabelStyle(XYGraphWidget.Edge.LEFT).setFormat(new Format() {
             // converts the Long received from the X-axis value into a date, and then neatly formats it
             @Override
             public StringBuffer format(Object obj, StringBuffer toAppendTo, FieldPosition pos) {
@@ -138,5 +139,17 @@ public class UserLogActivity extends AppCompatActivity {
                 throw new UnsupportedOperationException("Not yet implemented.");
             }
         });
+    }
+
+    /**
+     * Setup code for the money chart (formatting, incorporating data)
+     * @param moneyXYPlot
+     */
+    public void onCreateMoneyXYPlot(XYPlot moneyXYPlot) {
+
+    }
+
+    public void onCreateOrderLog(ListView ordersList) {
+
     }
 }
