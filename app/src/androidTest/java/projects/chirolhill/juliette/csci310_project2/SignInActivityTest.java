d841a000cd9f4b8bc5847bb44d55f57d8f337163
Cerina da Graca <dagraca@usc.edu>
2019-11-22 16:43:49 -0800

    basic test setup

--- app/src/androidTest/java/projects/chirolhill/juliette/csci310_project2/SignInActivityTest.java
+++ app/src/androidTest/java/projects/chirolhill/juliette/csci310_project2/SignInActivityTest.java
@@ -4,18 +4,20 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class SignInActivityTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
