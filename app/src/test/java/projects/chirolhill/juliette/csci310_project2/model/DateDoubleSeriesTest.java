c620895a7f88fa2830ca46fd687b7bb2f6716388
Nathan Chang <nathan98chang@gmail.com>
2019-11-24 14:56:38 -0800

    foregoing activity tests for now, doing unit tests for data model classes

---
+++ app/src/test/java/projects/chirolhill/juliette/csci310_project2/model/DateDoubleSeriesTest.java
@@ -0,0 +1,32 @@
+package projects.chirolhill.juliette.csci310_project2.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DateDoubleSeriesTest {
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void getTitle() {
+    }
+
+    @Test
+    public void getX() {
+    }
+
+    @Test
+    public void getY() {
+    }
+
+    @Test
+    public void size() {
+    }
+
+    @Test
+    public void getMaxYValue() {
+    }
+}
