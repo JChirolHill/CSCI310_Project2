6a71c2f84deebb34c82d79ce3a9d1cbaa3f76b4f
Nathan Chang <nathan98chang@gmail.com>
2019-11-24 12:34:50 -0800

    more boilerplate

--- app/src/androidTest/java/projects/chirolhill/juliette/csci310_project2/testLogActivity/LogActivityTestParseCaffeine.java
+++ app/src/androidTest/java/projects/chirolhill/juliette/csci310_project2/testLogActivity/LogActivityTestParseCaffeine.java
@@ -8,19 +8,27 @@ import static org.junit.Assert.*;
 
 public class LogActivityTestParseCaffeine {
 
+    /**
+     * Sign in to existing user, 'a@a.com'.
+     * @throws Exception
+     */
     @Before
     public void setUp() throws Exception {
+
     }
 
     @After
     public void tearDown() throws Exception {
+
     }
 
     @Test
     public void onCreate() {
+
     }
 
     @Test
     public void onActivityResult() {
+
     }
 }
