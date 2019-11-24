99e1ad6c590a9c95e2294a4cf109525fd0cf62df
Nathan Chang <nathan98chang@gmail.com>
2019-11-24 12:00:44 -0800

    first test, making template

---
+++ app/src/androidTest/java/projects/chirolhill/juliette/csci310_project2/testLogActivity/LogActivityTestParseCaffeine.java
@@ -0,0 +1,26 @@
+package projects.chirolhill.juliette.csci310_project2.testLogActivity;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LogActivityTestParseCaffeine {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void onActivityResult() {
+    }
+}
