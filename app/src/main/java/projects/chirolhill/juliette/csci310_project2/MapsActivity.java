e85a529941df01da4775822f0a9366e0d1601a93
Nathan Chang <nathan98chang@gmail.com>
2019-12-08 18:32:17 -0800

    jk, realized that could get annoying if user is moving around in a small radius

--- app/src/main/java/projects/chirolhill/juliette/csci310_project2/MapsActivity.java
+++ app/src/main/java/projects/chirolhill/juliette/csci310_project2/MapsActivity.java
@@ -71,6 +71,7 @@ public class MapsActivity extends FragmentActivity implements
         {
     private final String TAG = MapsActivity.class.getSimpleName();
 
+    private Button btnFindShops;
     private ImageButton imgProfile;
     private LocationManager locationManager;
     private Location myLocation;
